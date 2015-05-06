3e7c3d5f3c3c38fdab913101af4b71f5f7b9f1d2
david crosson <david.crosson@orange.com>
2015-05-06 17:26:15 +0200

    various fixes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Jenkins slave container
 
+Usage :
+* from the same host where the jenkins master is running
+  - `docker run -d -e SLAVE_NAME=S1 --link myjenkins:master --name myslave-1 dacr/jenkins-slave`
+* from an external host 
+  - `docker run -d -e SLAVE_NAME=S1 -e MASTER_URL=http://somewhere:someport/ --name slave-1 dacr/jenkins-slave`
+
--- swarm-client.sh
+++ swarm-client.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-java -jar $SWARM_CLIENT_JAR
+if [ -z "$SLAVE_NAME" ] ; then
+  SLAVE_NAME="default"
+fi
+
+if [ -z "$MASTER_URL" ] ; then
+  MASTER_URL=${MASTER_PORT/tcp/http}
+fi
+
+if [ -n "$MASTER_URL" ] ; then 
+  java -jar $SWARM_CLIENT_JAR -fsroot $JENKINS_HOME -name "$SLAVE_NAME" -master $MASTER_URL
+else
+  java -jar $SWARM_CLIENT_JAR -fsroot $JENKINS_HOME -name "$SLAVE_NAME"
+fi
+
 
