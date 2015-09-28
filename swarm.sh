1208a1b66b09551b2c53682ceb8004cf13788226
Crosson David <david.crosson@orange.com>
2015-09-28 14:51:40 +0200

    support jenkins master authentication

--- swarm.sh
+++ swarm.sh
@@ -8,10 +8,18 @@ if [ -z "$MASTER_URL" ] ; then
   MASTER_URL=${MASTER_PORT/tcp/http}
 fi
 
+if [ -n "$JENKINS_USERNAME" ] ; then
+  JENKINS_AUTH_OPTS="-username $JENKINS_USERNAME"
+fi
+
+if [ -n "$JENKINS_PASSWORD" ] ; then
+  JENKINS_AUTH_OPTS=$JENKINS_AUTH_OPTS" -passwordEnvVariable JENKINS_PASSWORD"
+fi
+
 if [ -n "$MASTER_URL" ] ; then 
-  java -jar $SWARM_CLIENT_JAR -fsroot $JENKINS_HOME -name "$SLAVE_NAME" -master $MASTER_URL "$@"
+  java -jar $SWARM_CLIENT_JAR -fsroot $JENKINS_HOME -name "$SLAVE_NAME" -master $MASTER_URL $JENKINS_AUTH_OPTS "$@"
 else
-  java -jar $SWARM_CLIENT_JAR -fsroot $JENKINS_HOME -name "$SLAVE_NAME" "$@"
+  java -jar $SWARM_CLIENT_JAR -fsroot $JENKINS_HOME -name "$SLAVE_NAME" $JENKINS_AUTH_OPTS "$@"
 fi
 
 
